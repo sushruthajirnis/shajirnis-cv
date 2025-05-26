cfabfa19ec00a1b109bea669de136a94e8323f18
Sushrut Hajirnis <sushrut.hajirnis@gmail.com>
2025-05-26 00:25:28 -0700

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -78,7 +78,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: 'assets/resume.pdf',
+      href: 'resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
